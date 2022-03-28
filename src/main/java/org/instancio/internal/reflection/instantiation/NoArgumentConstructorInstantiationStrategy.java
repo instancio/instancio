add34ef822bbc3b2143c93a8ee754a324e6d4f9b
Arman Sharif <armandino@gmail.com>
2022-03-27 20:39:40 -0700

    Boolean/character generator tests

--- src/main/java/org/instancio/internal/reflection/instantiation/NoArgumentConstructorInstantiationStrategy.java
+++ src/main/java/org/instancio/internal/reflection/instantiation/NoArgumentConstructorInstantiationStrategy.java
@@ -15,13 +15,9 @@
  */
 package org.instancio.internal.reflection.instantiation;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.lang.reflect.Constructor;
 
 public class NoArgumentConstructorInstantiationStrategy implements InstantiationStrategy {
-    private static final Logger LOG = LoggerFactory.getLogger(NoArgumentConstructorInstantiationStrategy.class);
 
     @Override
     @SuppressWarnings({"unchecked", "java:S3011"})
