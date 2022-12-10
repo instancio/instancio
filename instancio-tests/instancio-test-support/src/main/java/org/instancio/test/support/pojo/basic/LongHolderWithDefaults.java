c505629ef6b1a71113de9f0dab7046280e3fc9f5
Arman Sharif <armandino@gmail.com>
2022-12-09 17:20:33 -0800

    Added tests for a class with initialised fields.
    Verifies OVERWRITE_EXISTING_VALUES [enabled|disabled]
    with all populate actions + ignored() fields.

---
+++ instancio-tests/instancio-test-support/src/main/java/org/instancio/test/support/pojo/basic/LongHolderWithDefaults.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.instancio.test.support.pojo.basic;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@SuppressWarnings("FieldMayBeFinal")
+public class LongHolderWithDefaults {
+    public static final int PRIMITIVE = -111;
+    public static final long WRAPPER = -222L;
+
+    private long primitive;
+    private Long wrapper;
+
+    public LongHolderWithDefaults() {
+        this.primitive = PRIMITIVE;
+        this.wrapper = WRAPPER;
+    }
+
+    public long getPrimitive() {
+        return primitive;
+    }
+
+    public Long getWrapper() {
+        return wrapper;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+}
