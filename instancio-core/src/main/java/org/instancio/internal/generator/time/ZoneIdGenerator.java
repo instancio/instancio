933a7a83ad96134ab8d55816907d059d8a18519b
Arman Sharif <armandino@gmail.com>
2022-12-29 14:49:55 -0800

    ZoneId and ZoneOffset generators.
    Resolves #195, #201

---
+++ instancio-core/src/main/java/org/instancio/internal/generator/time/ZoneIdGenerator.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.instancio.internal.generator.time;
+
+import org.instancio.Random;
+import org.instancio.generator.GeneratorContext;
+import org.instancio.internal.generator.AbstractGenerator;
+
+import java.time.ZoneId;
+
+public class ZoneIdGenerator extends AbstractGenerator<ZoneId> {
+
+    public ZoneIdGenerator(final GeneratorContext context) {
+        super(context);
+    }
+
+    @Override
+    public ZoneId generate(final Random random) {
+        return ZoneId.of(random.oneOf(ZoneId.getAvailableZoneIds()));
+    }
+}
