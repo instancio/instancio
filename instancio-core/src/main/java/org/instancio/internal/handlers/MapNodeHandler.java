488e24129ec943ed2c9b8e8aa6c3cee00085b64f
Arman Sharif <armandino@gmail.com>
2023-06-20 22:32:55 -0700

    Refactor/cleanup node handlers

--- instancio-core/src/main/java/org/instancio/internal/handlers/MapNodeHandler.java
+++ instancio-core/src/main/java/org/instancio/internal/handlers/MapNodeHandler.java
@@ -21,9 +21,8 @@ import org.instancio.internal.context.ModelContext;
 import org.instancio.internal.generator.GeneratorResult;
 import org.instancio.internal.generator.util.MapGenerator;
 import org.instancio.internal.nodes.InternalNode;
+import org.instancio.internal.nodes.NodeKind;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Map;
 
 public class MapNodeHandler implements NodeHandler {
 
@@ -41,7 +40,7 @@ public class MapNodeHandler implements NodeHandler {
     @NotNull
     @Override
     public GeneratorResult getResult(@NotNull final InternalNode node) {
-        if (Map.class.isAssignableFrom(node.getTargetClass())) {
+        if (node.is(NodeKind.MAP)) {
             final MapGenerator<?, ?> generator = new MapGenerator<>(
                     new GeneratorContext(context.getSettings(), context.getRandom()));
 
