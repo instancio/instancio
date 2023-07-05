c626dc0fe9345b996f0262bd98150657af8b5b92
Arman Sharif <armandino@gmail.com>
2023-07-05 08:21:45 -0700

    Move test to correct package

--- instancio-tests/instancio-core-tests/src/test/java/org/instancio/settings/AssignmentTypeTest.java
+++ instancio-tests/instancio-core-tests/src/test/java/org/instancio/settings/AssignmentTypeTest.java
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.instancio.assignment;
+package org.instancio.settings;
 
-import org.instancio.settings.AssignmentType;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
