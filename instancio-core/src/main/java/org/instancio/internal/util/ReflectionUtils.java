86b73e679e3692d967f3214f315e66aab1606aaa
Arman Sharif <armandino@gmail.com>
2023-12-14 20:28:08 -0800

    Updated log statement to reduce noise

--- instancio-core/src/main/java/org/instancio/internal/util/ReflectionUtils.java
+++ instancio-core/src/main/java/org/instancio/internal/util/ReflectionUtils.java
@@ -17,6 +17,8 @@ package org.instancio.internal.util;
 
 import org.instancio.exception.InstancioException;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -24,6 +26,7 @@ import java.lang.reflect.Modifier;
 import java.util.Objects;
 
 public final class ReflectionUtils {
+    private static final Logger LOG = LoggerFactory.getLogger(ReflectionUtils.class);
 
     private ReflectionUtils() {
         // non-instantiable
@@ -33,7 +36,7 @@ public final class ReflectionUtils {
         try {
             return Class.forName(fullyQualifiedName);
         } catch (Exception ex) {
-            ExceptionUtils.logException("Could not load class: '{}'", ex, fullyQualifiedName);
+            LOG.trace("Class not found: '{}'", fullyQualifiedName);
             return null;
         }
     }
