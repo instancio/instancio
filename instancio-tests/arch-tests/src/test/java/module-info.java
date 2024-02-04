1ef37db736e297ad0400e7155c2e680a2b4e2801
Tim Feuerbach <webseiten.designer@googlemail.com>
2024-02-04 11:58:44 +0100

    Added JPMS module information

---
+++ instancio-tests/arch-tests/src/test/java/module-info.java
@@ -0,0 +1,8 @@
+open module org.instancio.test.contract {
+    requires org.assertj.core;
+    requires org.junit.jupiter;
+    requires com.tngtech.archunit;
+
+    requires org.instancio.core;
+    requires org.instancio.test.support;
+}
