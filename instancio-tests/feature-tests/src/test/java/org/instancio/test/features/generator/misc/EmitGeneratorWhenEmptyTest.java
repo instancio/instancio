2b8d2bef85609a1ab45e863a03bfe35072eda225
Arman Sharif <armandino@gmail.com>
2024-02-10 19:41:30 -0800

    `emit()` support for recycling items (#905)

--- instancio-tests/feature-tests/src/test/java/org/instancio/test/features/generator/misc/EmitGeneratorWhenEmptyTest.java
+++ instancio-tests/feature-tests/src/test/java/org/instancio/test/features/generator/misc/EmitGeneratorWhenEmptyTest.java
@@ -15,6 +15,7 @@
  */
 package org.instancio.test.features.generator.misc;
 
+import lombok.Data;
 import org.instancio.Instancio;
 import org.instancio.InstancioApi;
 import org.instancio.exception.InstancioApiException;
@@ -25,6 +26,7 @@ import org.instancio.settings.Settings;
 import org.instancio.test.support.pojo.basic.IntegerHolder;
 import org.instancio.test.support.tags.Feature;
 import org.instancio.test.support.tags.FeatureTag;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -33,6 +35,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.instancio.Select.all;
+import static org.instancio.Select.field;
 
 @FeatureTag({Feature.GENERATOR, Feature.EMIT_GENERATOR})
 @ExtendWith(InstancioExtension.class)
@@ -98,5 +101,88 @@ class EmitGeneratorWhenEmptyTest {
         assertThatThrownBy(api::create)
                 .isExactlyInstanceOf(InstancioApiException.class)
                 .hasMessageContaining("no item is available to emit()");
+    }
+
+    @Nested
+    class EmitRecycleTest {
+        @Test
+        void withoutUnused() {
+            final List<Integer> result = Instancio.ofList(Integer.class)
+                    .size(SIZE)
+                    .generate(all(Integer.class), gen -> gen.emit()
+                            .items(-1, -2, -3, -4, -5)
+                            .whenEmptyRecycle())
+                    .create();
+
+            assertThat(result)
+                    .hasSize(SIZE)
+                    .startsWith(-1, -2, -3, -4, -5, -1, -2, -3, -4, -5);
+        }
+
+        @Test
+        void withUnused_shouldFail() {
+            final InstancioApi<List<Integer>> api = Instancio.ofList(Integer.class)
+                    .size(SIZE)
+                    .generate(all(Integer.class), gen -> gen.emit()
+                            .items(-1, -2, -3)
+                            .whenEmptyRecycle());
+
+            assertThatThrownBy(api::create)
+                    .isExactlyInstanceOf(InstancioApiException.class)
+                    .hasMessageContaining("Remaining items: [-2, -3]");
+        }
+
+        @Test
+        void whenEmptyRecycleIgnoringUnused() {
+            final List<Integer> result = Instancio.ofList(Integer.class)
+                    .size(SIZE)
+                    .generate(all(Integer.class), gen -> gen.emit()
+                            .items(-1, -2, -3)
+                            .whenEmptyRecycle()
+                            .ignoreUnused())
+                    .create();
+
+            assertThat(result)
+                    .hasSize(SIZE)
+                    .startsWith(-1, -2, -3, -1, -2, -3, -1, -2, -3, -1);
+        }
+
+        @Test
+        void withNulls() {
+            final List<Integer> result = Instancio.ofList(Integer.class)
+                    .size(SIZE)
+                    .generate(all(Integer.class), gen -> gen.emit()
+                            .items(null, null)
+                            .whenEmptyRecycle()
+                            .ignoreUnused())
+                    .create();
+
+            assertThat(result).hasSize(SIZE).containsNull();
+        }
+
+        @Test
+        void withNestedLists() {
+            //@formatter:off
+            @Data class D2 { String value; }
+            @Data class D1 { String value; List<D2> depth2Nodes; }
+            @Data class Root { List<D1> depth1Nodes; }
+            //@formatter:on
+
+            final Root result = Instancio.of(Root.class)
+                    .generate(field(Root::getDepth1Nodes), gen -> gen.collection().size(2))
+                    .generate(field(D1::getValue), gen -> gen.emit().items("A", "B"))
+                    .generate(field(D1::getDepth2Nodes), gen -> gen.collection().size(3))
+                    .generate(field(D2::getValue), gen -> gen.emit().items("X", "Y", "Z").whenEmptyRecycle())
+                    .create();
+
+            assertThat(result.getDepth1Nodes())
+                    .hasSize(2)
+                    .allSatisfy(r -> assertThat(r.getDepth2Nodes())
+                            .hasSize(3)
+                            .extracting(D2::getValue)
+                            .containsExactly("X", "Y", "Z"))
+                    .extracting(D1::getValue)
+                    .containsExactly("A", "B");
+        }
     }
 }
