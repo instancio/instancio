instancio/instancio

commit 6695dae90836358640d2a9cefc9ae301933a5d13

Support for `ElementType.TYPE_USE` bean validation annotations (#911)

instancio-core/src/main/java/org/instancio/internal/annotation/AnnotationExtractor.java
@@ -17,11 +17,15 @@ package org.instancio.internal.annotation;
 
 import org.instancio.internal.context.ModelContext;
 import org.instancio.internal.nodes.InternalNode;
+import org.instancio.internal.util.CollectionUtils;
 import org.instancio.settings.BeanValidationTarget;
 import org.instancio.settings.Keys;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.reflect.AnnotatedParameterizedType;
+import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
@@ -41,7 +45,7 @@ final class AnnotationExtractor {
         final Field field = node.getField();
 
         if (field == null) {
-            return EMPTY_ANNOTATIONS;
+            return getTypeUseAnnotations(node);
         }
 
         if (beanValidationTarget == BeanValidationTarget.FIELD) {
@@ -51,5 +55,24 @@ final class AnnotationExtractor {
         // Get constraint annotations from the getter, if exists
         final Method getter = getterMethodResolver.getGetter(node);
         return getter == null ? EMPTY_ANNOTATIONS : getter.getDeclaredAnnotations();
+    }
+
+    /**
+     * Extract field's {@link ElementType#TYPE_USE} annotations,
+     * e.g. {@code Map<@Email String, @Negative Integer>}.
+     */
+    private static Annotation[] getTypeUseAnnotations(final InternalNode node) {
+        final Field parentField = node.getParent().getField();
+
+        if (parentField != null && parentField.getAnnotatedType() instanceof AnnotatedParameterizedType) {
+            final AnnotatedParameterizedType apt = (AnnotatedParameterizedType) parentField.getAnnotatedType();
+            final int childIndex = CollectionUtils.identityIndexOf(node, node.getParent().getChildren());
+
+            if (childIndex != -1) {
+                final AnnotatedType[] annotatedTypes = apt.getAnnotatedActualTypeArguments();
+                return annotatedTypes[childIndex].getAnnotations();
+            }
+        }
+        return EMPTY_ANNOTATIONS;
     }
 }

instancio-core/src/main/java/org/instancio/internal/util/CollectionUtils.java
@@ -16,6 +16,7 @@
 package org.instancio.internal.util;
 
 import org.instancio.Random;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
@@ -80,6 +81,17 @@ public final class CollectionUtils {
         final List<T> result = new ArrayList<>(list);
         Collections.addAll(result, values);
         return Collections.unmodifiableList(result);
+    }
+
+    // same as List.indexOf() but using '==' instead of equals()
+    @SuppressWarnings("PMD.CompareObjectsWithEquals")
+    public static int identityIndexOf(final Object obj, @NotNull final List<?> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == obj) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static <T> void shuffle(final Collection<T> collection, final Random random) {

instancio-tests/bean-validation-jakarta-tests/src/main/java/org/instancio/test/pojo/beanvalidation/CollectionBV.java
@@ -15,8 +15,9 @@
  */
 package org.instancio.test.pojo.beanvalidation;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -26,7 +27,7 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 
-public class CollectionSizeBV {
+public class CollectionBV {
 
     @Data
     public static class WithMinSize {
@@ -68,5 +69,12 @@ public class CollectionSizeBV {
         @NotNull
         @Size(min = 5, max = 5)
         private Queue<String> value;
+    }
+
+    @Data
+    public static class TypeUse {
+        @NotNull
+        @Size(min = 3, max = 3)
+        private Collection<@NotNull @Digits(integer = 7, fraction = 5) CharSequence> value;
     }
 }

instancio-tests/bean-validation-jakarta-tests/src/main/java/org/instancio/test/pojo/beanvalidation/MapBV.java
@@ -15,6 +15,7 @@
  */
 package org.instancio.test.pojo.beanvalidation;
 
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -27,7 +28,7 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.UUID;
 
-public class MapSizeBV {
+public class MapBV {
 
     @Data
     public static class WithMinSize {
@@ -69,5 +70,13 @@ public class MapSizeBV {
         @NotNull
         @Size(min = 5, max = 5)
         private Map<Long, Double> value;
+    }
+
+    @Data
+    public static class TypeUse {
+        @NotNull
+        private Map<
+                @NotNull @Digits(integer = 7, fraction = 5) CharSequence,
+                @NotNull @Digits(integer = 3, fraction = 2) CharSequence> value;
     }
 }

instancio-tests/bean-validation-jakarta-tests/src/test/java/org/instancio/test/beanvalidation/CollectionBVTest.java
@@ -20,60 +20,67 @@ import org.instancio.junit.InstancioExtension;
 import org.instancio.junit.WithSettings;
 import org.instancio.settings.Keys;
 import org.instancio.settings.Settings;
-import org.instancio.test.pojo.beanvalidation.MapSizeBV;
+import org.instancio.test.pojo.beanvalidation.CollectionBV;
 import org.instancio.test.support.tags.Feature;
 import org.instancio.test.support.tags.FeatureTag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.instancio.test.support.util.Constants.SAMPLE_SIZE_D;
+import static org.instancio.test.support.util.Constants.SAMPLE_SIZE_DD;
 
 @FeatureTag(Feature.BEAN_VALIDATION)
 @ExtendWith(InstancioExtension.class)
-class MapSizeBVTest {
+class CollectionBVTest {
 
     @WithSettings
     private final Settings settings = Settings.create()
             .set(Keys.STRING_NULLABLE, false)
-            .set(Keys.STRING_ALLOW_EMPTY, false)
-            .set(Keys.LONG_NULLABLE, false)
-            .set(Keys.CHARACTER_NULLABLE, false)
-            .set(Keys.DOUBLE_NULLABLE, false)
             .set(Keys.STRING_MAX_LENGTH, 10);
 
-    @Test
+    @RepeatedTest(SAMPLE_SIZE_DD)
     void withMinSize() {
-        final MapSizeBV.WithMinSize result = Instancio.create(MapSizeBV.WithMinSize.class);
+        final CollectionBV.WithMinSize result = Instancio.create(CollectionBV.WithMinSize.class);
         assertThat(result.getValue()).hasSizeGreaterThanOrEqualTo(8);
     }
 
-    @Test
+    @RepeatedTest(SAMPLE_SIZE_DD)
     void withMinSizeZeo() {
-        final MapSizeBV.WithMinSizeZero result = Instancio.create(MapSizeBV.WithMinSizeZero.class);
-        assertThat(result.getValue()).hasSizeBetween(0, Keys.MAP_MAX_SIZE.defaultValue());
+        final CollectionBV.WithMinSizeZero result = Instancio.create(CollectionBV.WithMinSizeZero.class);
+        assertThat(result.getValue()).hasSizeBetween(0, Keys.COLLECTION_MAX_SIZE.defaultValue());
     }
 
-    @Test
+    @RepeatedTest(SAMPLE_SIZE_DD)
     void withMaxSize() {
-        final MapSizeBV.WithMaxSize result = Instancio.create(MapSizeBV.WithMaxSize.class);
+        final CollectionBV.WithMaxSize result = Instancio.create(CollectionBV.WithMaxSize.class);
         assertThat(result.getValue()).hasSizeLessThanOrEqualTo(1);
     }
 
-    @Test
+    @RepeatedTest(SAMPLE_SIZE_DD)
     void withMaxSizeZero() {
-        final MapSizeBV.WithMaxSizeZero result = Instancio.create(MapSizeBV.WithMaxSizeZero.class);
+        final CollectionBV.WithMaxSizeZero result = Instancio.create(CollectionBV.WithMaxSizeZero.class);
         assertThat(result.getValue()).isEmpty();
     }
 
-    @Test
+    @RepeatedTest(SAMPLE_SIZE_DD)
     void withMinMaxSize() {
-        final MapSizeBV.WithMinMaxSize result = Instancio.create(MapSizeBV.WithMinMaxSize.class);
+        final CollectionBV.WithMinMaxSize result = Instancio.create(CollectionBV.WithMinMaxSize.class);
         assertThat(result.getValue()).hasSizeBetween(19, 20);
     }
 
-    @Test
+    @RepeatedTest(SAMPLE_SIZE_DD)
     void withMinMaxEqual() {
-        final MapSizeBV.WithMinMaxEqual result = Instancio.create(MapSizeBV.WithMinMaxEqual.class);
+        final CollectionBV.WithMinMaxEqual result = Instancio.create(CollectionBV.WithMinMaxEqual.class);
         assertThat(result.getValue()).hasSize(5);
+    }
+
+    @RepeatedTest(SAMPLE_SIZE_D)
+    void typeUse() {
+        final CollectionBV.TypeUse result = Instancio.create(CollectionBV.TypeUse.class);
+
+        assertThat(result.getValue())
+                .hasSize(3)
+                .allSatisfy(v -> assertThat(v).matches("\\d{7}\\.\\d{5}"));
     }
 }

instancio-tests/bean-validation-jakarta-tests/src/test/java/org/instancio/test/beanvalidation/MapBVTest.java
@@ -20,57 +20,72 @@ import org.instancio.junit.InstancioExtension;
 import org.instancio.junit.WithSettings;
 import org.instancio.settings.Keys;
 import org.instancio.settings.Settings;
-import org.instancio.test.pojo.beanvalidation.CollectionSizeBV;
+import org.instancio.test.pojo.beanvalidation.MapBV;
 import org.instancio.test.support.tags.Feature;
 import org.instancio.test.support.tags.FeatureTag;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.instancio.test.support.util.Constants.SAMPLE_SIZE_D;
 import static org.instancio.test.support.util.Constants.SAMPLE_SIZE_DD;
 
 @FeatureTag(Feature.BEAN_VALIDATION)
 @ExtendWith(InstancioExtension.class)
-class CollectionSizeBVTest {
+class MapBVTest {
 
     @WithSettings
     private final Settings settings = Settings.create()
             .set(Keys.STRING_NULLABLE, false)
+            .set(Keys.STRING_ALLOW_EMPTY, false)
+            .set(Keys.LONG_NULLABLE, false)
+            .set(Keys.CHARACTER_NULLABLE, false)
+            .set(Keys.DOUBLE_NULLABLE, false)
             .set(Keys.STRING_MAX_LENGTH, 10);
 
     @RepeatedTest(SAMPLE_SIZE_DD)
     void withMinSize() {
-        final CollectionSizeBV.WithMinSize result = Instancio.create(CollectionSizeBV.WithMinSize.class);
+        final MapBV.WithMinSize result = Instancio.create(MapBV.WithMinSize.class);
         assertThat(result.getValue()).hasSizeGreaterThanOrEqualTo(8);
     }
 
     @RepeatedTest(SAMPLE_SIZE_DD)
     void withMinSizeZeo() {
-        final CollectionSizeBV.WithMinSizeZero result = Instancio.create(CollectionSizeBV.WithMinSizeZero.class);
-        assertThat(result.getValue()).hasSizeBetween(0, Keys.COLLECTION_MAX_SIZE.defaultValue());
+        final MapBV.WithMinSizeZero result = Instancio.create(MapBV.WithMinSizeZero.class);
+        assertThat(result.getValue()).hasSizeBetween(0, Keys.MAP_MAX_SIZE.defaultValue());
     }
 
     @RepeatedTest(SAMPLE_SIZE_DD)
     void withMaxSize() {
-        final CollectionSizeBV.WithMaxSize result = Instancio.create(CollectionSizeBV.WithMaxSize.class);
+        final MapBV.WithMaxSize result = Instancio.create(MapBV.WithMaxSize.class);
         assertThat(result.getValue()).hasSizeLessThanOrEqualTo(1);
     }
 
     @RepeatedTest(SAMPLE_SIZE_DD)
     void withMaxSizeZero() {
-        final CollectionSizeBV.WithMaxSizeZero result = Instancio.create(CollectionSizeBV.WithMaxSizeZero.class);
+        final MapBV.WithMaxSizeZero result = Instancio.create(MapBV.WithMaxSizeZero.class);
         assertThat(result.getValue()).isEmpty();
     }
 
     @RepeatedTest(SAMPLE_SIZE_DD)
     void withMinMaxSize() {
-        final CollectionSizeBV.WithMinMaxSize result = Instancio.create(CollectionSizeBV.WithMinMaxSize.class);
+        final MapBV.WithMinMaxSize result = Instancio.create(MapBV.WithMinMaxSize.class);
         assertThat(result.getValue()).hasSizeBetween(19, 20);
     }
 
     @RepeatedTest(SAMPLE_SIZE_DD)
     void withMinMaxEqual() {
-        final CollectionSizeBV.WithMinMaxEqual result = Instancio.create(CollectionSizeBV.WithMinMaxEqual.class);
+        final MapBV.WithMinMaxEqual result = Instancio.create(MapBV.WithMinMaxEqual.class);
         assertThat(result.getValue()).hasSize(5);
+    }
+
+    @RepeatedTest(SAMPLE_SIZE_D)
+    void typeUse() {
+        final MapBV.TypeUse result = Instancio.create(MapBV.TypeUse.class);
+
+        assertThat(result.getValue()).allSatisfy((k, v) -> {
+            assertThat(k).matches("\\d{7}\\.\\d{5}");
+            assertThat(v).matches("\\d{3}\\.\\d{2}");
+        });
     }
 }

instancio-tests/bean-validation-javax-tests/src/main/java/org/instancio/test/pojo/beanvalidation/CollectionBV.java
@@ -15,59 +15,66 @@
  */
 package org.instancio.test.pojo.beanvalidation;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import lombok.Data;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NavigableMap;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.UUID;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
-public class MapSizeBV {
+public class CollectionBV {
 
     @Data
     public static class WithMinSize {
         @NotNull
         @Size(min = 8)
-        private Map<String, Long> value;
+        private Collection<String> value;
     }
 
     @Data
     public static class WithMinSizeZero {
         @NotNull
         @Size
-        private SortedMap<Integer, String> value;
+        private List<String> value;
     }
 
     @Data
     public static class WithMaxSize {
         @NotNull
         @Size(max = 1)
-        private HashMap<UUID, BigDecimal> value;
+        private Set<String> value;
     }
 
     @Data
     public static class WithMaxSizeZero {
         @NotNull
         @Size(max = 0)
-        private NavigableMap<Integer, String> value;
+        private ArrayList<String> value;
     }
 
     @Data
     public static class WithMinMaxSize {
         @NotNull
         @Size(min = 19, max = 20)
-        private TreeMap<String, Character> value;
+        private Deque<String> value;
     }
 
     @Data
     public static class WithMinMaxEqual {
         @NotNull
         @Size(min = 5, max = 5)
-        private Map<Long, Double> value;
+        private Queue<String> value;
+    }
+
+    @Data
+    public static class TypeUse {
+        @NotNull
+        @Size(min = 3, max = 3)
+        private Collection<@NotNull @Digits(integer = 7, fraction = 5) CharSequence> value;
     }
 }

instancio-tests/bean-validation-javax-tests/src/main/java/org/instancio/test/pojo/beanvalidation/MapBV.java
@@ -15,58 +15,68 @@
  */
 package org.instancio.test.pojo.beanvalidation;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Deque;
-import java.util.List;
-import java.util.Queue;
-import java.util.Set;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.UUID;
 
-public class CollectionSizeBV {
+public class MapBV {
 
     @Data
     public static class WithMinSize {
         @NotNull
         @Size(min = 8)
-        private Collection<String> value;
+        private Map<String, Long> value;
     }
 
     @Data
     public static class WithMinSizeZero {
         @NotNull
         @Size
-        private List<String> value;
+        private SortedMap<Integer, String> value;
     }
 
     @Data
     public static class WithMaxSize {
         @NotNull
         @Size(max = 1)
-        private Set<String> value;
+        private HashMap<UUID, BigDecimal> value;
     }
 
     @Data
     public static class WithMaxSizeZero {
         @NotNull
         @Size(max = 0)
-        private ArrayList<String> value;
+        private NavigableMap<Integer, String> value;
     }
 
     @Data
     public static class WithMinMaxSize {
         @NotNull
         @Size(min = 19, max = 20)
-        private Deque<String> value;
+        private TreeMap<String, Character> value;
     }
 
     @Data
     public static class WithMinMaxEqual {
         @NotNull
         @Size(min = 5, max = 5)
-        private Queue<String> value;
+        private Map<Long, Double> value;
+    }
+
+    @Data
+    public static class TypeUse {
+        @NotNull
+        private Map<
+                @NotNull @Digits(integer = 7, fraction = 5) CharSequence,
+                @NotNull @Digits(integer = 3, fraction = 2) CharSequence> value;
     }
 }

commit 110b9f5c30bf4bf022ab5cdb912e5c2315cf8464

Bug fix: bean validation NPE when creating a collection
Introduced in 6695dae (#911)

instancio-core/src/main/java/org/instancio/internal/annotation/AnnotationExtractor.java
@@ -62,7 +62,7 @@ final class AnnotationExtractor {
      * e.g. {@code Map<@Email String, @Negative Integer>}.
      */
     private static Annotation[] getTypeUseAnnotations(final InternalNode node) {
-        final Field parentField = node.getParent().getField();
+        final Field parentField = node.getParent() == null ? null : node.getParent().getField();
 
         if (parentField != null && parentField.getAnnotatedType() instanceof AnnotatedParameterizedType) {
             final AnnotatedParameterizedType apt = (AnnotatedParameterizedType) parentField.getAnnotatedType();

instancio-tests/bean-validation-hibernate-tests/src/main/java/org/instancio/test/pojo/beanvalidation/person/PetBV.java
@@ -16,6 +16,7 @@
 package org.instancio.test.pojo.beanvalidation.person;
 
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -27,6 +28,7 @@ public class PetBV {
     @Length(max = 10)
     private String name;
 
+    @Min(0)
     @Max(100)
     private int age;
 }

instancio-tests/bean-validation-hibernate-tests/src/test/java/org/instancio/test/beanvalidation/PersonBVTest.java
@@ -28,7 +28,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.instancio.test.support.util.Constants.SAMPLE_SIZE_DDD;
+import static org.instancio.test.support.util.Constants.SAMPLE_SIZE_DD;
 
 @FeatureTag(Feature.BEAN_VALIDATION)
 @ExtendWith(InstancioExtension.class)
@@ -36,7 +36,7 @@ class PersonBVTest {
 
     @Test
     void person() {
-        final int sampleSize = SAMPLE_SIZE_DDD;
+        final int sampleSize = SAMPLE_SIZE_DD;
 
         final List<PersonBV> results = Instancio.of(PersonBV.class)
                 .stream()
@@ -45,6 +45,15 @@ class PersonBVTest {
 
         assertThat(results)
                 .hasSize(sampleSize)
+                .allSatisfy(HibernateValidatorUtil::assertValid);
+    }
+
+    @Test
+    void personList() {
+        final List<PersonBV> results = Instancio.createList(PersonBV.class);
+
+        assertThat(results)
+                .isNotEmpty()
                 .allSatisfy(HibernateValidatorUtil::assertValid);
     }
 }