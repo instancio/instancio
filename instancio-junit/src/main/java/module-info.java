464b29f089a71370eeb887fed23be61ba40addff
Arman Sharif <armandino@gmail.com>
2024-02-20 21:02:36 -0800

    Modified `org.junit.jupiter.params` as optional in module-info

--- instancio-junit/src/main/java/module-info.java
+++ instancio-junit/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module org.instancio.junit {
   requires transitive org.instancio.core;
 
+  requires static org.junit.jupiter.params;
+
   requires org.junit.jupiter.api;
-  requires org.junit.jupiter.params;
   requires org.slf4j;
 
   exports org.instancio.junit;
