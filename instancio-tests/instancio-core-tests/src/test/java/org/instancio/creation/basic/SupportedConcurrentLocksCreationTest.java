81725d25f40d87a9fff8089bb7d63ea5ed59bfe8
Arman Sharif <armandino@gmail.com>
2024-02-27 08:23:43 -0800

    Tests for `java.util.concurrent.locks.*`

---
+++ instancio-tests/instancio-core-tests/src/test/java/org/instancio/creation/basic/SupportedConcurrentLocksCreationTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2022-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.instancio.creation.basic;
+
+import org.instancio.test.support.pojo.basic.SupportedConcurrentLocksTypes;
+import org.instancio.testsupport.templates.AutoVerificationDisabled;
+import org.instancio.testsupport.templates.CreationTestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@AutoVerificationDisabled
+public class SupportedConcurrentLocksCreationTest extends CreationTestTemplate<SupportedConcurrentLocksTypes> {
+
+    @Override
+    protected void verify(final SupportedConcurrentLocksTypes result) {
+        assertThat(result.getReentrantLock()).isNotNull();
+        assertThat(result.getReentrantReadWriteLock()).isNotNull();
+        assertThat(result.getStampedLock()).isNotNull();
+
+        // Currently not supported
+        assertThat(result.getLock()).isNull();
+    }
+}
