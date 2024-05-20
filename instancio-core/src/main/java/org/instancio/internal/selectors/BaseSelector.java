530dc32799e24a21c7ebd5ea0e151f05f43a1a4e
Arman Sharif <armandino@gmail.com>
2024-05-19 22:49:30 -0700

    Minor refactoring: extract selector base class

---
+++ instancio-core/src/main/java/org/instancio/internal/selectors/BaseSelector.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.instancio.internal.selectors;
+
+import org.instancio.Scope;
+import org.instancio.TargetSelector;
+import org.instancio.internal.util.Format;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+
+abstract class BaseSelector implements UnusedSelectorDescription, InternalSelector {
+
+    private final List<Scope> scopes;
+    private final Throwable stackTraceHolder;
+    private final boolean isLenient;
+
+    BaseSelector(final List<Scope> scopes, final Throwable stackTraceHolder, final boolean isLenient) {
+        this.scopes = Collections.unmodifiableList(scopes);
+        this.stackTraceHolder = stackTraceHolder;
+        this.isLenient = isLenient;
+    }
+
+    @NotNull
+    @Override
+    public final List<Scope> getScopes() {
+        return scopes;
+    }
+
+    @Override
+    public final List<TargetSelector> flatten() {
+        return Collections.singletonList(this);
+    }
+
+    @Override
+    public final boolean isLenient() {
+        return isLenient;
+    }
+
+    @Override
+    public final String getDescription() {
+        return String.format("%s%n    at %s", this, Format.firstNonInstancioStackTraceLine(getStackTraceHolder()));
+    }
+
+    public final Throwable getStackTraceHolder() {
+        return stackTraceHolder;
+    }
+}
