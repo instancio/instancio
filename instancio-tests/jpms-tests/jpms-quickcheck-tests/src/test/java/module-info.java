fd643f7bc2bd915ca8edefa0f0a3d1e3fbf9ccec
Arman Sharif <armandino@gmail.com>
2024-06-14 20:10:14 -0700

    Removed deprecated instancio-quickcheck module

--- instancio-tests/jpms-tests/jpms-quickcheck-tests/src/test/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module org.instancio.tests.jpms.quikcheck {
-    requires org.instancio.core;
-    requires org.instancio.quickcheck;
-
-    requires org.assertj.core;
-    requires org.junit.jupiter.api;
-    requires org.junit.jupiter.engine;
-}
