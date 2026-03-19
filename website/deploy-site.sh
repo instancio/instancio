f14a7f9bc8f5b1816a3eccf6191c1eef4196598d
Arman Sharif <arman@thinkthru.ca>
2026-03-18 21:12:09 -0400

    Update deploy site script

--- website/deploy-site.sh
+++ website/deploy-site.sh
@@ -6,5 +6,5 @@ git init
 git checkout -b gh-pages
 git add .
 git commit -m "Deploy site"
-git push -f "https://`db-get GHP_TOKEN`@github.com/instancio/instancio" gh-pages
+git push -f "git@github.com:instancio/instancio.git" HEAD:gh-pages
 echo "Done"
