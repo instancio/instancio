ab6c7cb62ed219dfcb6afa957a01fab6156cc820
Arman Sharif <arman@thinkthru.ca>
2026-03-23 21:29:08 -0700

    Refactor: factor out generator processing logic

--- instancio-core/src/main/java/org/instancio/internal/generator/SpiGeneratorResolver.java
+++ instancio-core/src/main/java/org/instancio/internal/generator/SpiGeneratorResolver.java
@@ -19,16 +19,13 @@ import org.instancio.generator.AfterGenerate;
 import org.instancio.generator.Generator;
 import org.instancio.generator.GeneratorContext;
 import org.instancio.generator.GeneratorSpec;
-import org.instancio.generator.Hints;
 import org.instancio.generators.Generators;
 import org.instancio.internal.context.ModelContext;
-import org.instancio.internal.generator.array.ArrayGenerator;
 import org.instancio.internal.generator.misc.GeneratorDecorator;
 import org.instancio.internal.generators.BuiltInGenerators;
 import org.instancio.internal.nodes.InternalNode;
 import org.instancio.internal.spi.ProviderEntry;
 import org.instancio.internal.util.Sonar;
-import org.instancio.internal.util.Verify;
 import org.instancio.settings.Keys;
 import org.instancio.spi.InstancioServiceProvider.GeneratorProvider;
 import org.instancio.spi.InstancioSpiException;
@@ -38,7 +35,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-import static org.instancio.internal.util.ObjectUtils.defaultIfNull;
+import static java.util.Objects.requireNonNullElse;
 
 public class SpiGeneratorResolver {
     private static final Logger LOG = LoggerFactory.getLogger(SpiGeneratorResolver.class);
@@ -79,40 +76,12 @@ public class SpiGeneratorResolver {
         return null;
     }
 
-    /**
-     * Follows almost the same logic as {@code UserSuppliedGeneratorProcessor}
-     * with some minor differences. Ideally, this should be refactored.
-     */
-    // TODO refactor
     @SuppressWarnings(Sonar.GENERIC_WILDCARD_IN_RETURN)
     private Generator<?> processGenerator(final Generator<?> generator, final InternalNode node) {
-        if (generator instanceof ArrayGenerator<?> arrayGenerator) {
-            arrayGenerator.subtype(node.getTargetClass());
-        } else if (generator instanceof final AbstractGenerator<?> g) {
-
-            if (!g.isDelegating()) {
-                return g;
-            }
-
-            final Hints hints = Verify.notNull(generator.hints(), "SPI generator hints are null");
-            final InternalGeneratorHint internalHint = Verify.notNull(hints.get(InternalGeneratorHint.class),
-                    "InternalGeneratorHint is null");
-
-            final Generator<?> delegate = generatorResolver.getCachedBuiltInGenerator(
-                    defaultIfNull(internalHint.targetClass(), node.getTargetClass()));
-
-            if (delegate == null) {
-                return generator;
-            }
-
-            if (delegate instanceof AbstractGenerator<?> delegateGenerator) {
-                final boolean nullable = g.isNullable();
-                delegateGenerator.nullable(nullable);
-            }
-            return GeneratorDecorator.replaceHints(delegate, hints);
-        }
-
-        return generator;
+        return DelegatingGeneratorProcessor.process(generator, node, internalHint -> {
+            final Class<?> targetClass = requireNonNullElse(internalHint.targetClass(), node.getTargetClass());
+            return generatorResolver.getCachedBuiltInGenerator(targetClass);
+        });
     }
 
     private static void validateSpec(
