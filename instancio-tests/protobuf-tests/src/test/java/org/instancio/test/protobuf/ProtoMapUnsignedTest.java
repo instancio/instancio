91a161a77414e9d89b785671f5cba81fb3b97d2f
Arman Sharif <arman@thinkthru.ca>
2026-09-17 21:52:11 -0700

    Fix unsigned number generation for repeated protobuf fields (#1861)

---
+++ instancio-tests/protobuf-tests/src/test/java/org/instancio/test/protobuf/ProtoMapUnsignedTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.instancio.test.protobuf;
+
+import org.instancio.Instancio;
+import org.instancio.junit.InstancioExtension;
+import org.instancio.junit.WithSettings;
+import org.instancio.settings.Keys;
+import org.instancio.settings.Settings;
+import org.instancio.test.prototobuf.Proto;
+import org.instancio.test.support.util.Constants;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(InstancioExtension.class)
+class ProtoMapUnsignedTest {
+
+    @WithSettings
+    private static final Settings SETTINGS = Settings.create()
+            .set(Keys.INTEGER_MIN, Integer.MIN_VALUE)
+            .set(Keys.INTEGER_MAX, Integer.MAX_VALUE)
+            .set(Keys.LONG_MIN, Long.MIN_VALUE)
+            .set(Keys.LONG_MAX, Long.MAX_VALUE);
+
+    @RepeatedTest(Constants.SAMPLE_SIZE_DDD)
+    void unsignedMapValues_shouldGenerateNonNegativeValues() {
+        final Proto.SupportedNumericTypesAsMapValues result =
+                Instancio.create(Proto.SupportedNumericTypesAsMapValues.class);
+
+        assertThat(result.getUnsignedIntsMap()).isNotEmpty().allSatisfy((k, v) -> assertThat(v).isNotNegative());
+        assertThat(result.getFixedIntsMap()).isNotEmpty().allSatisfy((k, v) -> assertThat(v).isNotNegative());
+        assertThat(result.getUnsignedLongsMap()).isNotEmpty().allSatisfy((k, v) -> assertThat(v).isNotNegative());
+        assertThat(result.getFixedLongsMap()).isNotEmpty().allSatisfy((k, v) -> assertThat(v).isNotNegative());
+    }
+
+    @RepeatedTest(Constants.SAMPLE_SIZE_DDD)
+    void unsignedMapKeys_shouldGenerateNonNegativeKeys() {
+        final Proto.SupportedNumericTypesAsMapKeys result =
+                Instancio.create(Proto.SupportedNumericTypesAsMapKeys.class);
+
+        assertThat(result.getUnsignedIntKeysMap()).isNotEmpty().allSatisfy((k, v) -> assertThat(k).isNotNegative());
+        assertThat(result.getFixedIntKeysMap()).isNotEmpty().allSatisfy((k, v) -> assertThat(k).isNotNegative());
+        assertThat(result.getUnsignedLongKeysMap()).isNotEmpty().allSatisfy((k, v) -> assertThat(k).isNotNegative());
+        assertThat(result.getFixedLongKeysMap()).isNotEmpty().allSatisfy((k, v) -> assertThat(k).isNotNegative());
+    }
+}
